70ce53af492d58474e279809f6121a4d5fe3f80f
Payten Giles <payten.giles@gmail.com>
2020-06-25 13:11:06 +1000

    Expose the new repository data in the Repository Details accordion on resource/ao records

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -6,11 +6,41 @@ Rails.application.config.after_initialize do
       result = render_pre_yale_repositories(context, options)
 
       if options[:partial] == "repositories/repository"
+        # display on the repo show template
         result += render(context, :partial => 'repositories/repository_ext',
-                                  :locals => options[:locals])
+                                  :locals => options[:locals].merge({
+                                    :show_map => true,
+                                    :show_opening_hours => true,
+                                  }))
+
+      elsif options[:partial] == "repositories/full_repo"
+        # display in the Repository Detail accordion
+        result += render(context, :partial => 'repositories/repository_ext',
+                         :locals => options[:locals].merge({
+                           :result => options.dig(:locals, :info, 'top') || {},
+                           :show_map => false,
+                           :show_opening_hours => true,
+                         }))
       end
 
       result
+    end
+  end
+
+  Record.class_eval do
+    # Include the new repo fields in the parsed repository object that is
+    # used about the place
+    alias_method :parse_repository_info_pre_yale_repositories, :parse_repository_info
+    def parse_repository_info
+      info = parse_repository_info_pre_yale_repositories
+
+      unless resolved_repository.nil?
+        info['top']['libcal_id'] = resolved_repository['libcal_id']
+        info['top']['latitude'] = resolved_repository['latitude']
+        info['top']['longitude'] = resolved_repository['longitude']
+      end
+
+      info
     end
   end
 
